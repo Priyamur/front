f4b9f10ddeecf23a49fe10c0536b8bb2c68d877e
Priyamur <144650236+Priyamur@users.noreply.github.com>
2024-05-03 16:58:47 +0530

    Update Register.js

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -135,16 +135,16 @@ export default function Register() {
 
 
     return (
-        <div style={{height:"100vh"}} class="register">
+        <div class="register">
             <div class="row">
                 <div class="col-md-3 register-left">
                     <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt="" />
                     <h3>Welcome to Relevantz </h3>
                     <h4>Learning Management System</h4>
-                    <h6>Gain your knowledge with Relevantz</h6>
+                    <h6>gain your knowledge with Relevantz</h6>
                     <input type="submit" name="" value="Login" /><br />
                 </div>
-                <div  class="col-md-9 register-right">
+                <div class="col-md-9 register-right">
                     <div class="tab-content" id="myTabContent">
                         <div class="tab-pane fade show active " id="home" role="tabpanel" aria-labelledby="home-tab">
                             <h3 class="register-heading">Registration</h3>
@@ -163,38 +163,30 @@ export default function Register() {
                                         {errors.password && <div className="text-danger">{errors.password}</div>}
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" placeholder="Confirm Password *" value={confirmPassword} name="confirmPassword" />
+                                        <input type="password" class="form-control" placeholder="Confirm Password *" value={confirmPassword} name="confirmPassword" onChange={(e) => setConfirmPassword(e.target.value)} />
                                     </div>
-                                    <h6 id="gender1">Gender:</h6>  
                                     <div class="form-group">
-                                        
                                         <div class="maxl">
-                        
-                                       
-                                        <div id="gender">  <label  class="gender radio inline">
-                                            
+                                            <label class="gender radio inline">
                                                 <input type="radio" name="gender" value="male" checked={gender === "Male"} onChange={handleGenderChange} />
                                                 <span> Male </span>
                                             </label>
-                                            <label  class="gender radio inline">
+                                            <label class="gender radio inline">
                                                 <input type="radio" name="gender" value="female" checked={gender === "Female"} onChange={handleGenderChange} />
                                                 <span>Female </span>
                                             </label>
-                                            <label  class="gender radio inline">
-                                                <input type="radio" name="gender" value="female" checked={gender === "Others"} onChange={handleGenderChange} />
-                                                <span>Others </span>
-                                            </label>
-                                            </div>
                                         </div>
                                         {errors.gender && <div className="text-danger">{errors.gender}</div>}
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <input type="number" class="form-control" placeholder="Phone Number *" value="" />
+                                        <input type="number" class="form-control" placeholder="Phone Number *"  value={contactNumber} name="phonenumber" onChange={(e) => setContactNumber(e.target.value)} />
+                                        {errors.contactNumber && <div className="text-danger">{errors.contactNumber}</div>}
                                     </div>
                                     <div class="form-group">
-                                        <input type="date" class="form-control" placeholder="Date Of Birth *" value="" />
+                                        <input type="date" class="form-control" placeholder="Date Of Birth *"  value={dob} name="dob" onChange={(e) => setDob(e.target.value)} />
+                                        {errors.dob && <div className="text-danger">{errors.dob}</div>}
                                     </div>
                                     <div class="form-group">
                                         <MultiSelectDropdown />
@@ -202,18 +194,17 @@ export default function Register() {
 
                                     <div class="form-group d-flex">
                                         <input type="email" class="form-control" placeholder="Your Email *"value={email} onChange={(e) => setEmail(e.target.value)} />
-                                        {errors.email && <div className="text-danger">{errors.email}</div>}
+                                        
                                     {/* Green tick symbol for verified email */}
                                     {emailVerified && <span className="text-success">&#10004;</span>}
                                     {/* Button to send OTP */}
-                                        <button className='btn btn-primary' onClick={handleSendOTP}>Send Otp</button>
+                                        {!emailVerified && <button className='btn btn-primary' onClick={handleSendOTP}>Send Otp</button>}
                                     </div>
                                     {showOTP && (<div class="form-group">
                                         <input type="text" minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Enter OTP *" value="" />
                                     </div>)}
-                                    <br></br>
-
-                                    <input style={{marginRight:"80%"}} type="submit" class="btnRegister" value="Register" />
+                                    {errors.email && <div className="text-danger">{errors.email}</div>}
+                                    <input type="submit" class="btnRegister" value="Register" onClick={handleSubmit} />
                                 </div>
                             </div>
 
